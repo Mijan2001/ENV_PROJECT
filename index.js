b6a0575c6c1e6910c62ffc03ba2c5a4834d622dc
mijan <mijan.cse19@gmail.com>
2024-02-17 18:43:49 +0600

    first Commit

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello I am home route");
-});
-
-app.listen(PORT, () => {
-  console.log(`server is running http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`this app is runnig at port : ${port}`);
 });
